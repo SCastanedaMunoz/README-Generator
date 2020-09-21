3ca066d7117631661f687e69cf48d8cfe41d14cc
Santiago Castaneda Munoz <santiagocm98@hotmail.com>
2020-09-20 23:11:33 -0500

    Adding Work on Licenses Badges

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // array of questions for user
@@ -22,7 +23,7 @@ const questions = [
         type: "input",
         message: "Describe the project's usage information",
         name: "usage"
-    }, 
+    },
     {
         type: "input",
         message: "Describe the project's contribution guidelines",
@@ -34,9 +35,18 @@ const questions = [
         name: "tests"
     },
     {
-        type: "input",
+        type: "list",
         message: "What's your project's license?",
-        name: "license"
+        name: "license",
+        choices: [
+            { name: "Apache", value: { name: "Apache", badge: "https://img.shields.io/badge/License-Apache%202.0-blue.svg" } },
+            { name: "Creative Commons", value: { name: "Creative Commons", badge: "https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg" } },
+            { name: "GNU GPLv3", value: { name: "GNU GPLv3", badge: "https://img.shields.io/badge/License-GPLv3-blue.svg" } },
+            { name: "ISC", value: { name: "ISC", badge: "https://img.shields.io/badge/License-ISC-blue.svg" } },
+            { name: "MIT", value: { name: "MIT", badge: "https://img.shields.io/badge/License-MIT-yellow.svg" } },
+            { name: "Unlicense", value: { name: "Unlicense", badge: "https://img.shields.io/badge/License-WTFPL-brightgreen.svg" } },
+            { name: "Zlib", value: { name: "Zlib", badge: "https://img.shields.io/badge/License-Zlib-lightgrey.svg" } }
+        ]
     },
     {
         type: "input",
@@ -45,24 +55,58 @@ const questions = [
     },
     {
         type: "input",
-        message: "Enter your email adress",
+        message: "Enter your name",
+        name: "devname"
+    },
+    {
+        type: "input",
+        message: "Enter your Email adress",
         name: "email"
+    },
+    {
+        type: "input",
+        message: "How to contact in case of questions?",
+        name: "questioning"
+    },
+    {
+        type: "input",
+        message: "How would you like to name your README file?",
+        name: "filename"
     }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
 
+    const outputsDir = "./outputs";
+
+    if (!fs.existsSync(outputsDir)) {
+        fs.mkdir(outputsDir, err => {
+            if (err) {
+                console.log(`Error: ${err}`)
+                return;
+            }
+            console.log("Outputs Directory Not Found, Creating a New One!");
+        });
+    }
+
+    fs.writeFile(`${outputsDir}/${fileName}.md`, data, "utf-8", err => {
+        if (err) {
+            console.log(`Error: ${err}`)
+            return;
+        }
+        console.log(`${fileName}.md saved to outputs folder!`);
+    });
 }
 
 // function to initialize program
 function init() {
     inquirer
-    .prompt(questions)
-    .then(function (response) {
-        var markdown = generateMarkdown(response);
-        console.log(markdown);
-    });
+        .prompt(questions)
+        .then(function (response) {
+            var markdown = generateMarkdown(response);
+            writeToFile(response.filename, markdown);
+        });
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,48 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}
-  
-    `;
+
+    console.log(data);
+
+    var markDownContent =
+`![License: ${data.license.name}](${data.license.badge})
+
+# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+1. [Description](##Description)
+2. [Installation](##Installation)
+3. [Usage](##Usage)
+4. [License](##License)
+5. [Contributing](##Contributing)
+6. [Tests](##Tests)
+7. [Questions](##Questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${data.license}
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.tests}
+
+## Questions
+${data.questioning}
+
+https://github.com/${data.username}
+
+Email: ${data.email}`;
+
+    return markDownContent;
   }
   
 module.exports = generateMarkdown;
